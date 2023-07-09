eca16758e100d3432656474f56717ad10d595369
hongsh429 <shh0429@kakao.com>
2023-07-09 23:08:00 +0900

    paging : 순수 jpa 확인
    spring data jpa는 각각의 db 방언에 맞게 쿼리를 날려준다.

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -58,4 +58,22 @@ public class MemberJpaRepository {
                 .setParameter("username", username)
                 .getResultList();
     }
+
+
+    /*
+    방언에 맞게 jpa가 알아서 세팅해서 각각의 db에 맞게 페이징 쿼리를 날려준다.
+    */
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
