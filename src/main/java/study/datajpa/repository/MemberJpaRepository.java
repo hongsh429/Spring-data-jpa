818e14e7883ff160dd4328696ff71d15d2f8bec6
hongsh429 <shh0429@kakao.com>
2023-07-09 21:20:44 +0900

    Member - Team :  순수 JPA. 왜 Spring data jpa가 탄생하게 된 이유

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -18,5 +21,26 @@ public class MemberJpaRepository {
 
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em                    /* JPQL은 ENTITY 대상으로 db의 sql문과 비슷하게 만드는 것 */
+                .createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em
+                .createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
     }
 }
