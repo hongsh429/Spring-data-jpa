9471ac4fc4a3650266415be3693d0fc86c56c375
hongsh429 <shh0429@kakao.com>
2023-07-09 21:00:18 +0900

    Member - Team 연관관계 세팅 및 test코드 작성

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,25 +1,41 @@
 package study.datajpa.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // proxy 기술을 쓸 때, 사용한다.
+@ToString(of = {"id", "username", "age"}) // toString의 내용. 여기에 team 있다면, toString 무한 루프가 돈다.
 public class Member {
 
     @Id
     @GeneratedValue // 식별자 값을 디비에 알아서 넣어준다.
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // Memmber + Team(proxy) -> Team을 조회하면, 그때 team select 쿼리가 날라감.
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team); // 연관관계 세팅 메서드
+        }
+    }
+
+    // == 연관관계 세팅 메서드 == // -> 이건 연관관계의 주인에게 있는 것이 맞다. 왜? 수정이 가능한 곳!
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,29 @@
+package study.datajpa.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // proxy 기술을 쓸 때, 사용한다.
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id
+    @GeneratedValue // 식별자 값을 디비에 알아서 넣어준다.
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
